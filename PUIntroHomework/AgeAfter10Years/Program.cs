51b2a6b2e67bca020cecdbaf7af475aa92d09395
yoanapaligorova <joanapal17@gmail.com>
2018-10-02 16:03:45 +0300

    included that homework 1 & 2

---
+++ PUIntroHomework/AgeAfter10Years/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 18;
+            int after10Yaers = myAge + 10;
+            Console.WriteLine("My age after 10 years =>!"+ after10Yaers); 
+        }
+    }
+}
---
+++ PUIntroHomework/TheHiProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheHiProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Yoana");
+        }
+    }
+}
