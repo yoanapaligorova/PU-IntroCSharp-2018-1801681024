1f351ba56498234d47b5c12c7e584198cd12d01d
joanapal17 <joanapal17@gmail.com>
2018-10-07 17:54:43 +0300

    Included homework week1

---
+++ PUIntroHomework/MyUniqueNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyUniqueNumber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("How old are you?");
+			int age = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the last 4 digits of your faculty number here:");
+			int last4Digits = int.Parse(Console.ReadLine());
+
+			double result = MyUniqueNumber(AgeAfter10Years(age), last4Digits);
+
+			Console.WriteLine("After 10 years you will be " + AgeAfter10Years(age) + " years old. " +
+				"And your unique number is: " + result);
+		}
+
+		static int AgeAfter10Years(int currentAge)
+		{
+			int ageAfter10Years = 10 + currentAge;
+
+			return ageAfter10Years;
+		}
+
+		static double MyUniqueNumber(int ageAfter10Years, int last4DigitsOfFacNumber)
+		{
+			double myUniqueNumber = (ageAfter10Years * last4DigitsOfFacNumber) / Math.PI;
+			return myUniqueNumber;
+		}
+	}
+}
