7365bafaaa63a99e258e0c5eee71a84e3a95e99a
yoanapaligorova <joanapal17@gmail.com>
2018-10-09 16:07:55 +0300

    Commited travel to Sun

---
+++ PUIntroHomework/Travel2TheSunWithSoundSpeedInWater/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Travel2TheSunWithSoundSpeedInWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int distance2SunFromEarth = 149600000;
+			short speedOfSound20DegreeWater = 1481;
+			double timeNeeded = (double)distance2SunFromEarth / speedOfSound20DegreeWater;
+
+
+            Console.WriteLine($"Time needed:{timeNeeded}");
+
+        }
+    }
+}
