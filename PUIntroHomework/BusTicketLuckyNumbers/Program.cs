3f6964f31a613548f166baff6fecfa51cdd5d6f4
joanapal17 <joanapal17@gmail.com>
2018-10-21 15:33:25 +0300

    Included homework 3 for week 3

---
+++ PUIntroHomework/BusTicketLuckyNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BusTicketLuckyNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int luckyTicketsCount = 0;
+			int unluckyTicketsCount = 0;
+			for (int i = 0; i < 1000000; i++)
+			{
+				if ((i / 100000 + i % 100000 / 10000 + i % 100000 % 10000 / 1000) == (i % 100000 % 10000 % 1000 / 100 + i % 100000 % 10000 % 1000 % 100 / 10 + i % 10))
+				{
+					Console.WriteLine(i);
+					luckyTicketsCount++;
+				}
+				else
+				{
+					unluckyTicketsCount++;
+				}
+			}
+			Console.WriteLine("The number of lucky tickets is: " + luckyTicketsCount);
+			Console.WriteLine("The number of unlucky tickets is: " + unluckyTicketsCount);
+			Console.WriteLine($"Total tickets: {luckyTicketsCount + unluckyTicketsCount}");
+		}
+	}
+}
---
+++ PUIntroHomework/MiserPiggy/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MiserPiggy
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int sumPiggyName1 = 0;
+			int sumPiggyName2 = 0;
+			int sumPiggyName3 = 0;
+
+			
+			Console.WriteLine("Hello, piggies! Enter your names:");
+			Console.Write("Piggy 1: ");
+			string piggyName1 = Console.ReadLine();
+			Console.Write("Piggy 2: ");
+			string piggyName2 = Console.ReadLine();
+			Console.Write("Piggy 3: ");
+			string piggyName3 = Console.ReadLine();
+
+			
+			foreach (char a in piggyName1)
+			{
+				sumPiggyName1 += a;
+			}
+
+			foreach (char b in piggyName2)
+			{
+				sumPiggyName2 += b;
+			}
+
+			foreach (char c in piggyName3)
+			{
+				sumPiggyName3 += c;
+			}
+
+			
+			if (sumPiggyName1 > sumPiggyName2 && sumPiggyName1 > sumPiggyName3)
+			{
+				if (sumPiggyName2 > sumPiggyName3)
+				{
+					Console.WriteLine($"The name-wealthiest pig is {piggyName1} ({sumPiggyName1}) followed by " +
+						$"{piggyName2} ({sumPiggyName2}) and last is {piggyName3} ({sumPiggyName3}).");
+				}
+				else
+				{
+					Console.WriteLine($"The name-wealthiest pig is {piggyName1} ({sumPiggyName1}) followed by " +
+						$"{piggyName3} ({sumPiggyName3}) and last is {piggyName2} ({sumPiggyName2}).");
+				}
+			}
+			else if (sumPiggyName2 > sumPiggyName1 && sumPiggyName2 > sumPiggyName3)
+			{
+				if (sumPiggyName1 > sumPiggyName3)
+				{
+					Console.WriteLine($"The name-wealthiest pig is {piggyName2} ({sumPiggyName2}) followed by " +
+						$"{piggyName1} ({sumPiggyName1}) and last is {piggyName3} ({sumPiggyName3}).");
+				}
+				else
+				{
+					Console.WriteLine($"The name-wealthiest pig is {piggyName2} ({sumPiggyName2}) followed by " +
+						$"{piggyName3} ({sumPiggyName3}) and last is {piggyName1} ({sumPiggyName1}).");
+				}
+			}
+			else if (sumPiggyName1 > sumPiggyName2)
+			{
+				Console.WriteLine($"The name-wealthiest pig is {piggyName3} ({sumPiggyName3}) followed by " +
+					$"{piggyName1} ({sumPiggyName1}) and last is {piggyName2} ({sumPiggyName2}).");
+			}
+			else
+			{
+				Console.WriteLine($"The name-wealthiest pig is {piggyName3} ({sumPiggyName3}) followed by " +
+					$"{piggyName2} ({sumPiggyName2}) and last is {piggyName1} ({sumPiggyName1}).");
+			}
+		}
+	}
+}
